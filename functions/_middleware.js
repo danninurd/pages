33df38ed20638079090c33341c79866b14f6cf53
danninurd <80585513+danninurd@users.noreply.github.com>
2025-08-05 19:57:38 +0700

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,17 @@
+// Ganti URL ini dengan URL website sumber yang kontennya ingin Anda tampilkan
+const SOURCE_URL = "https://google.com";
+
+export const onRequest = async (context) => {
+  const url = new URL(context.request.url);
+  const newUrl = new URL(url.pathname, SOURCE_URL);
+
+  const newRequest = new Request(newUrl, {
+    method: context.request.method,
+    headers: context.request.headers,
+    body: context.request.body,
+    redirect: 'follow'
+  });
+
+  const response = await fetch(newRequest);
+  return response;
+};
